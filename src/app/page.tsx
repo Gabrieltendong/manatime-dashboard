34c31ef8762b2986300e057b80b85ae55e8d3743
Gabriel <gabitendong@gmail.com>
2023-10-15 23:59:57 +0100

    update next.config.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
+import { Dashboard } from "@mui/icons-material";
 import Head from "next/head";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Manitime administration dashboard</title>
-        <meta name="description" content="Manitime administration dashboard" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main></main>
-    </div>
-  );
+  return <Dashboard />;
 }
