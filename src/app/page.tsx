1c7b4f9c14ea4d8444338228280934cc85ae168b
Gabriel <gabitendong@gmail.com>
2023-10-16 00:30:19 +0100

    update next.config.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Manitime administration dashboard</title>
         <meta name="description" content="Manitime administration dashboard" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main></main>
     </div>
   );
 }
